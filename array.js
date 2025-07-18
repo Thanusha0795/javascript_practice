359a91d003f964c776edc9207bbce346b5f47e0b
Thanusha0795 <kunche002@gmail.com>
2025-07-18 12:20:09 +0530

    array_change

---
+++ array.js
@@ -0,0 +1,41 @@
+// // Array
+
+// var name =["pandu","chinni","ammu"];
+// console.log(name);
+// console.log(name[2]);
+
+// //new array
+// var name1 = new Array ("pandu","chinni","ammu");
+// console.log(name1);
+// console.log(name1[1]);
+// console.log(name1.length);
+
+// // Array Methods
+// let fruits = ["apple", "banana", "Orange",]
+// console.log(fruits.length);
+// console.log(fruits);
+// //1.push() Add elements to the end of the array
+// fruits.push("mango");
+// console.log(fruits);
+// console.log(fruits);
+
+// console.log(fruits.length);
+// //2.concat()/
+// fruits.concat("kiwi","grape");
+// console.log(fruits);
+// console.log(fruits.length);
+// //3.unshift()
+// fruits.unshift(("grape"));
+// console.log(fruits);
+// console.log(fruits.length);
+//Array Methods for getting elements [gets the index of specified element in the arrary]
+// let int = [1,2,3,4]
+// let a = int.indexOf(2);  //.indexOf(2);
+// console.log(a);
+ 
+//  console.log(int.indexOf(5));
+
+// const array = [1,2,3,4,5]
+// let c = array.find((num) => num %2 ==0); // find()
+// console.log(c);
+
--- function.js
+++ function.js
@@ -1,5 +1,61 @@
-function add(a, b) {
-    let sum = a + b
-    return sum
-} console.log(add(5, 4))
+// Function example and named function
 
+function product(a,b){
+    return a * b
+}
+console.log(product(5,7));
+
+// function exression & named function expression
+var product = function product (a,b){
+    d = a+b;
+    e = a-b;
+    f = a*b;
+    g = a/b;
+    return {d, e,f,g};
+}
+  
+console.log(product(6,6));
+
+
+// anonymous function expresssion
+let add = function (a,b){
+    let d = a+b;
+    let e = a-b;
+    let f = a*b;
+    let g = a/b;
+    return {d, e,f,g}; 
+}
+console.log(add(6,9));
+
+// //anonymous function
+// function (a,b){
+//     let d = a+b;
+//     let e = a-b;
+//     let f = a*b;
+//     let g = a/b;
+//     return {d, e,f,g};
+// }(6,6);   //undefind
+
+// Arrow Function
+const sum = (a,b) => {
+    var d = a+b;
+    return d;
+}
+console.log(sum(9,2));
+
+// Arrow function with single statement 
+const sum2 = (a,b) => a+b;
+console.log(sum2(9,4));
+
+// Arrow function with single parameter
+const square = a => a*a;
+console.log(square(5));
+
+const cube  = a  => a*a*a;
+console.log(cube(2));
+
+// no arguments
+var greet =()=> "Hellow good morning";
+console.log(greet());
+var send = () => console.log("Hello Gold");
+send();
